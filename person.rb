431253fdf865668e065c769ecccdbe3fa1332f66
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-04-17 21:10:15 +0100

    fix linter error

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_accessor :id, :name, :age, :parent_permission
-    
-    def initialize(age, name = 'Unknown', parent_permission = true)
+  attr_accessor :id, :name, :age, :parent_permission
+  
+  def initialize(age, name = 'Unknown', parent_permission: true)
       @id = Random.rand(1..200)
       @age = age
       @parent_permission = parent_permission
       @name = name
     end
-    
-    private def of_age?
+
+    private 
+  def of_age?
       age >= 18
     end
     
-    public def can_use_services?
+    public
+  def can_use_services?
       of_age? || parent_permission
     end
   end
-  
- 
   
--- student.rb
+++ student.rb
@@ -1,19 +1,14 @@
 require './person'
 
 class Student < Person
-    attr_reader :classroom
-    def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
         super(name, age, parent_permission)
         @classroom = classroom
     end
 
-    def play_hooky
+  def play_hooky
         "¯\(ツ)/¯"
     end
 end
- 
-
-
-
-
-
