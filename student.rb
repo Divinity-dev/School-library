83e8f8b9d575ccd2838f74f17b771eca261f17b9
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-04-17 21:22:02 +0100

    fix linter errors

--- student.rb
+++ student.rb
@@ -4,11 +4,11 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-        super(name, age, parent_permission)
-        @classroom = classroom
-    end
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
   def play_hooky
-        "¯\(ツ)/¯"
-    end
+    "¯\(ツ)/¯"
+  end
 end
