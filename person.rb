438a9d07c4a65295a92c7cfd816cadeed37f9d5f
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-04-18 08:23:27 +0100

    fix linter errors

--- person.rb
+++ person.rb
@@ -8,14 +8,15 @@ class Person
     @name = name
   end
 
-  private 
+  private
+
   def of_age?
     age >= 18
   end
-    
+
   public
+
   def can_use_services?
     of_age? || parent_permission
   end
 end
-  
