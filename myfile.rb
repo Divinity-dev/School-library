aeb78feef5c19f1aff5170be6733b4f4e46fecce
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-04-20 09:03:32 +0100

    Added rentals and classroom classes

--- myfile.rb
+++ myfile.rb
@@ -1,10 +1,21 @@
 require './person'
 require './capitalize_decorator'
 require './trimmer_decorator'
+require './classroom'
+require './rentals'
+require './book'
+require './student'
 
+# Create a Person
 person = Person.new(22, 'maximilianus')
+
+# Correct the person's name and print it
 person.correct_name
+puts "Corrected name: #{person.name}"
+
+# Decorate the person's name with capitalization and trimming decorators, and print the result
 capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
-p capitalized_trimmed_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts "Decorated name: #{capitalized_trimmed_person.correct_name}"
+
+
--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './nameable'
 
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,6 +9,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
